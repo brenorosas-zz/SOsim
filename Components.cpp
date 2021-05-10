f564ccca9166d77eadd3152b6b06666cc136f08d
leviwc <leviwc3@hotmail.com>
2021-05-10 15:02:07 -0300

    adicionando ram e disco

--- Components.cpp
+++ Components.cpp
@@ -1,8 +1,14 @@
 #include "header.cpp"
+#include "pages_algorithms.cpp"
+#include "process_algorithms.cpp"
 struct Pagina {
     int idPagina;
     int idProcessoAssociado;
-    Pagina(int idPagina, int idProcesso) { this->idPagina = idPagina; this->idProcessoAssociado = idProcesso;}
+    Pagina() {}
+    Pagina(int idPagina, int idProcesso) {
+        this->idPagina = idPagina;
+        this->idProcessoAssociado = idProcesso;
+    }
 };
 struct Processo {
     int tempoDeChegada;
@@ -26,4 +32,26 @@ struct Processo {
         }
     }
     bool pronto() { return paginasProntas == quantidadeDePaginas; }
+};
+struct DISCO {
+    vector<Pagina> disco;
+    DISCO(){};
+};
+struct RAM {
+    Pagina ram[50];
+    RAM() {
+        for (int i = 0; i < 50; i++) {
+            ram[i] = Pagina(i, -1);
+        }
+    }
+};
+struct MaquinaVirtual {
+    DISCO disco;
+    RAM ram;
+    vector<Processo> processos;
+    MaquinaVirtual() {
+        ram = RAM();
+        disco = DISCO();
+    }
+    void addProcesso(Processo processo) { processos.push_back(processo); }
 };
