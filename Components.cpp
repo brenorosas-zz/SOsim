611c6f6522c9133abcaf45d4a523e465e9ec9b85
leviwc <leviwc3@hotmail.com>
2021-05-10 12:44:31 -0300

    struct pagina e processo

---
+++ Components.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+struct Pagina {
+    int idPagina;
+    Pagina(int idPagina) { this->idPagina = idPagina; }
+};
+struct Processo {
+    int tempoDeChegada;
+    int tempoDeExecucao;
+    int deadline;
+    int prioridade;
+    int quantidadeDePaginas;
+    int idPagina;
+    vector<Pagina> paginas;
+    Processo(int tempoDeChegada, int tempoDeExecucao, int deadline,
+             int prioridade, int quantidadeDePaginas, int idPagina) {
+        this->tempoDeChegada = tempoDeChegada;
+        this->tempoDeExecucao = tempoDeExecucao;
+        this->deadline = deadline;
+        this->prioridade = prioridade;
+        this->quantidadeDePaginas = quantidadeDePaginas;
+        this->idPagina = idPagina;
+        for (int i = idPagina; i < idPagina + quantidadeDePaginas; i++) {
+            paginas.push_back(Pagina(i));
+        }
+    }
+};
