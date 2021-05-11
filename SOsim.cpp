aa96ad974bcd2b86d5e5e399bcbd171c44c44fec
leviwc <leviwc3@hotmail.com>
2021-05-11 11:54:00 -0300

    iniciando o main

--- SOsim.cpp
+++ SOsim.cpp
@@ -1,4 +1,16 @@
+#include "Components.cpp"
 #include "header.cpp"
-int32_t main(){
-
+int32_t main() {
+    int n;
+    cin >> n;
+    MaquinaVirtual maquina;
+    for (int i = 0; i < n; i++) {
+        int tempoDeChegada, tempoDeExecucao, deadline prioridade,
+            quantidadeDePaginas, idProcesso = i;
+        cin >> tempoDeChegada >> tempoDeExecucao >> deadline >> prioridade >>
+            quantidadeDePaginas;
+        maquina.addProcesso(Processo(tempoDeChegada, TempoDeExecucao, deadline,
+                                     prioridade, quantidadeDePaginas,
+                                     idProcesso));
+    }
 }
