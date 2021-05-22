ce752fd23f7415ab822568c6ab1f47da80cf6822
brenorosas <brenorosas@hotmail.com>
2021-05-22 17:43:18 -0300

    Round robin + Prints para entendimento

--- Components.cpp
+++ Components.cpp
@@ -1,19 +1,22 @@
 #include "header.cpp"
-#include "pages_algorithms.cpp"
-struct Pagina {
+struct Pagina
+{
     int idPagina;
     int idProcessoAssociado;
     Pagina() {}
-    Pagina(int idPagina, int idProcesso) {
+    Pagina(int idPagina, int idProcesso)
+    {
         this->idPagina = idPagina;
         this->idProcessoAssociado = idProcesso;
     }
-    bool operator==(const Pagina p) const {
+    bool operator==(const Pagina p) const
+    {
         return this->idPagina == p.idPagina and
                this->idProcessoAssociado == p.idProcessoAssociado;
     }
 };
-struct Processo {
+struct Processo
+{
     int tempoDeChegada;
     int tempoDeExecucao;
     int deadline;
@@ -21,70 +24,91 @@ struct Processo {
     int quantidadeDePaginas;
     int idProcesso;
     int paginasProntas = 0;
+    bool estourou = false;
     vector<Pagina> paginas;
     Processo(int tempoDeChegada, int tempoDeExecucao, int deadline,
-             int prioridade, int quantidadeDePaginas, int idProcesso) {
+             int prioridade, int quantidadeDePaginas, int idProcesso)
+    {
         this->tempoDeChegada = tempoDeChegada;
         this->tempoDeExecucao = tempoDeExecucao;
         this->deadline = deadline;
         this->prioridade = prioridade;
         this->quantidadeDePaginas = quantidadeDePaginas;
         this->idProcesso = idProcesso;
-        for (int i = 0; i < quantidadeDePaginas; i++) {
+        for (int i = 0; i < quantidadeDePaginas; i++)
+        {
             paginas.push_back(Pagina(i, idProcesso));
         }
     }
     bool pronto() { return paginasProntas == quantidadeDePaginas; }
 };
-struct DISCO {
+struct DISCO
+{
     vector<Pagina> disco;
     DISCO(){};
-    bool existe(Pagina pagina) {
+    bool existe(Pagina pagina)
+    {
         for (auto &paginaD : disco)
-            if (pagina == paginaD) return true;
+            if (pagina == paginaD)
+                return true;
         return false;
     }
     void add(Pagina pagina) { disco.push_back(pagina); }
-    void remove(Pagina pagina) {
-        for (int i = 0; i < disco.size(); i++) {
-            if (pagina == disco[i]) {
+    void remove(Pagina pagina)
+    {
+        for (int i = 0; i < disco.size(); i++)
+        {
+            if (pagina == disco[i])
+            {
+                cout << "Removendo do disco a pagina " << pagina.idPagina << ' ' << pagina.idProcessoAssociado << endl;
                 disco.erase(i + disco.begin());
                 break;
             }
         }
+        sleep(1);
     }
 };
-struct RAM {
+struct RAM
+{
     Pagina ram[50];
     int proximo = 0;
     vector<int> queueEDF;
-    RAM() {
-        for (int i = 0; i < 50; i++) {
+    RAM()
+    {
+        for (int i = 0; i < 50; i++)
+        {
             ram[i] = Pagina(i, -1);
             queueEDF.push_back(i);
         }
     }
-    bool existe(Pagina pagina) {
+    bool existe(Pagina pagina)
+    {
         for (int i = 0; i < 50; i++)
-            if (pagina == ram[i]) return true;
+            if (pagina == ram[i])
+                return true;
         return false;
     }
-    Pagina addFIFO(Pagina pagina) {
+    Pagina addFIFO(Pagina pagina)
+    {
         Pagina aux = ram[proximo];
         ram[proximo] = pagina;
         proximo = (proximo + 1) % 50;
         return aux;
     }
-    Pagina addMRU(Pagina pagina) {
+    Pagina addMRU(Pagina pagina)
+    {
         Pagina aux = ram[queueEDF[0]];
         ram[queueEDF[0]] = pagina;
         queueEDF.push_back(queueEDF[0]);
         queueEDF.erase(queueEDF.begin());
         return aux;
     }
-    void updateMRU(Pagina pagina) {
-        for (int i = 0; i < 50; i++) {
-            if (ram[queueEDF[i]] == pagina) {
+    void updateMRU(Pagina pagina)
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            if (ram[queueEDF[i]] == pagina)
+            {
                 queueEDF.push_back(queueEDF[i]);
                 queueEDF.erase(queueEDF.begin() + i);
                 break;
@@ -92,68 +116,104 @@ struct RAM {
         }
     }
 };
-struct MaquinaVirtual {
+struct MaquinaVirtual
+{
     DISCO disco;
     RAM ram;
+    int quantum;
     vector<Processo> processos;
     string escalonador, paginacao;
     int tempo;
-    MaquinaVirtual() {
+    MaquinaVirtual()
+    {
         ram = RAM();
         disco = DISCO();
         tempo = 0;
     }
     void addProcesso(Processo processo) { processos.push_back(processo); }
-    void page(vector<Pagina> &paginas) {
-        for (Pagina &pagina : paginas) {
-            if (!ram.existe(pagina)) {
+    void page(vector<Pagina> &paginas)
+    {
+        for (Pagina &pagina : paginas)
+        {
+            cout << "paginando pagina " << pagina.idPagina << ' ' << pagina.idProcessoAssociado << endl;
+            if (!ram.existe(pagina))
+            {
+                cout << "Buscando pagina " << pagina.idPagina << ' ' << pagina.idProcessoAssociado << " no disco" << endl;
                 Pagina aux;
-                if (paginacao == "FIFO") aux = ram.addFIFO(pagina);
-                if (paginacao == "MRU") aux = ram.addMRU(pagina);
-                if (aux.idPagina != -1) {
+                if (paginacao == "FIFO")
+                    aux = ram.addFIFO(pagina);
+                if (paginacao == "MRU")
+                    aux = ram.addMRU(pagina);
+                if (aux.idProcessoAssociado != -1)
+                {
+                    cout << "Removeu a pagina " << aux.idPagina << ' ' << aux.idProcessoAssociado << " da Ram" << endl;
                     disco.add(aux);
                 }
+                tempo++;
                 disco.remove(pagina);
-            } else if (paginacao == "MRU") {
+            }
+            else if (paginacao == "MRU")
+            {
                 ram.updateMRU(pagina);
             }
         }
     }
-    void execute_processo(Processo &processo, int tempo) {
+    void execute_processo(Processo &processo, int tempoDeExecucao)
+    {
+        cout << "Preparando processo " << processo.idProcesso << " para execução" << endl;
         page(processo.paginas);
         processo.paginasProntas = processo.paginas.size();
-        sleep(tempo * 1000);
+        cout << "Processo pronto para executar" << endl;
+        sleep(tempoDeExecucao);
+        cout << "Processo " << processo.idProcesso << " executado" << endl;
+        processo.tempoDeExecucao -= tempoDeExecucao;
+        tempo += tempoDeExecucao;
     }
-    bool cmpFIFO(Processo a, Processo b) {
+    static bool cmpFIFO(Processo a, Processo b)
+    {
         return a.tempoDeChegada < b.tempoDeChegada;
     }
-    void FIFO() {
-        if (processos.size() == 0) return;
-        sort(processos.begin(), processos.end(), cmpFIFO);
-        for (Processo &processo : processos) {
-            if (processo.tempoDeChegada < tempo) {
+    void FIFO()
+    {
+        if (processos.size() == 0)
+            return;
+        for (Processo &processo : processos)
+        {
+            if (processo.tempoDeChegada > tempo)
+            {
+                cout << "Aguardando próximo processo" << endl;
                 sleep(processo.tempoDeChegada - tempo);
+                tempo = processo.tempoDeChegada;
             }
             execute_processo(processo, processo.tempoDeExecucao);
-            tempo += processo.tempoDeExecucao;
+            if (tempo > processo.deadline)
+            {
+                processo.estourou = true;
+            }
         }
     }
-    struct cmpSJF : binary_function<Processo, Processo, bool> {
-        bool operator()(const Processo &a, const Processo &b) const {
-            return a.tempoDeExecucao < b.tempoDeExecucao;
+    struct cmpSJF : binary_function<Processo, Processo, bool>
+    {
+        bool operator()(const Processo &a, const Processo &b) const
+        {
+            return a.tempoDeExecucao > b.tempoDeExecucao;
         }
     };
-    void SJF() {
+    void SJF()
+    {
         int at = 0;
-        sort(processos.begin(), processos.end(), cmpFIFO);
         priority_queue<Processo, vector<Processo>, cmpSJF> queue;
-        while (!queue.empty() or at < processos.size() - 1) {
-            if (queue.empty() and tempo < processos[at].tempoDeChegada) {
+        while (!queue.empty() or at < processos.size() - 1)
+        {
+            if (queue.empty() and tempo < processos[at].tempoDeChegada)
+            {
+                cout << "Aguardando próximo processo" << endl;
                 sleep(processos[at].tempoDeChegada - tempo);
                 tempo = processos[at].tempoDeChegada;
             }
             while (at < processos.size() and
-                   processos[at].tempoDeChegada <= tempo) {
+                   processos[at].tempoDeChegada <= tempo)
+            {
                 queue.push(processos[at]);
                 at++;
             }
@@ -162,17 +222,45 @@ struct MaquinaVirtual {
             queue.pop();
         }
     }
-    void rodar() {
-        cin >> escalonador >> paginacao;
-        if (escalonador == "FIFO") {
-            FIFO();
+    void RoundRobin()
+    {
+        queue<Processo> fila;
+        for (Processo &processo : processos)
+            fila.push(processo);
+        while (!fila.empty())
+        {
+            Processo processo = fila.front();
+            fila.pop();
+            if (processo.tempoDeChegada > tempo)
+            {
+                cout << "Aguardando próximo processo" << endl;
+                sleep(processo.tempoDeChegada - tempo);
+                tempo = processo.tempoDeChegada;
+            }
+            execute_processo(processo, min(quantum, processo.tempoDeExecucao));
+            if (tempo > processo.deadline)
+            {
+                processo.estourou = true;
+            }
+            if (processo.tempoDeExecucao > 0)
+            {
+                fila.push(processo);
+            }
         }
-        // else if (escalonador == "SJF") {
-        //     SJF();
-        // } else if (escalonador == "RoundRobin") {
-        //     RoundRobin();
-        // } else if (escalonador == "EDF") {
-        //     EDF();
-        // }
+    }
+    void EDF()
+    {
+    }
+    void rodar()
+    {
+        sort(processos.begin(), processos.end(), cmpFIFO);
+        if (escalonador == "FIFO")
+            FIFO();
+        else if (escalonador == "SJF")
+            SJF();
+        else if (escalonador == "RoundRobin")
+            RoundRobin();
+        else if (escalonador == "EDF")
+            EDF();
     }
 };
--- SOsim.cpp
+++ SOsim.cpp
@@ -1,10 +1,12 @@
 #include "Components.cpp"
 #include "header.cpp"
-int32_t main() {
+int32_t main()
+{
     int n;
     cin >> n;
     MaquinaVirtual maquina = MaquinaVirtual();
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         int tempoDeChegada, tempoDeExecucao, deadline, prioridade,
             quantidadeDePaginas, idProcesso = i;
         cin >> tempoDeChegada >> tempoDeExecucao >> deadline >> prioridade >>
@@ -13,4 +15,10 @@ int32_t main() {
                                      prioridade, quantidadeDePaginas,
                                      idProcesso));
     }
+    cin >> maquina.paginacao >> maquina.escalonador;
+    if (maquina.escalonador == "RoundRobin")
+    {
+        cin >> maquina.quantum;
+    }
+    maquina.rodar();
 }
